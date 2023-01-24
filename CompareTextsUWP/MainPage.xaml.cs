f1e3962a430da0387e6e8a354e4c8b27016d9e03
Hans <hk66dev@outlook.com>
2023-01-24 19:31:58 +0100

    Lite av varje:).

--- CompareTextsUWP/MainPage.xaml.cs
+++ CompareTextsUWP/MainPage.xaml.cs
@@ -51,95 +51,21 @@ namespace CompareTextsUWP
         private void Text1_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateMainPage();
-
-            //// Check if rows in text 1 is present in text 2. Display difference, format the output as desired "ResultDisplayMode
-            //CompareTwoStrings compStrings2 = new CompareTwoStrings(Text1.Text.Trim(), Text2.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult1 = new StringHelper();
-            //Result1.Text = shResult1.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode1.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Check if rows in text 2 is present in text 1. Display difference.
-            //compStrings2 = new CompareTwoStrings(Text2.Text.Trim(), Text1.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult2 = new StringHelper();
-            //Result2.Text = shResult2.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode2.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Display statistics of text 1
-            //StringHelper strInfo = new StringHelper(Text1.Text);
-            //StatText1.Text = strInfo.ToString();
-
-            //// Display statistics of result 1
-            //StringHelper strStatResult1 = new StringHelper(Result1.Text);
-            //StatResult1.Text = strStatResult1.ToString();
-            //// Display statistics of result 2
-            //StringHelper strStatResult2 = new StringHelper(Result2.Text);
-            //StatResult2.Text = strStatResult2.ToString();
         }
 
         private void Text2_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateMainPage();
-
-            //// Check if rows in text 1 is present in text 2. Display difference.
-            //CompareTwoStrings compStrings2 = new CompareTwoStrings(Text1.Text.Trim(), Text2.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult1 = new StringHelper();
-            //Result1.Text = shResult1.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode1.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Check if rows in text 2 is present in text 1. Display difference.
-            //compStrings2 = new CompareTwoStrings(Text2.Text.Trim(), Text1.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult2 = new StringHelper();
-            //Result2.Text = shResult2.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode2.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Display statistics of text 2
-            //StringHelper strInfo = new StringHelper(Text2.Text);
-            //StatText2.Text = strInfo.ToString();
-
-            //// Display statistics of result 1
-            //StringHelper strStatResult1 = new StringHelper(Result1.Text);
-            //StatResult1.Text = strStatResult1.ToString();
-            //// Display statistics of result 2
-            //StringHelper strStatResult2 = new StringHelper(Result2.Text);
-            //StatResult2.Text = strStatResult2.ToString();
         }
 
         private void Text1_Loaded(object sender, RoutedEventArgs e)
         {
             UpdateMainPage();
-
-            //// Check if rows in text 1 is present in text 2. Display difference.
-            //CompareTwoStrings compStrings2 = new CompareTwoStrings(Text1.Text.Trim(), Text2.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult1 = new StringHelper();
-            //Result1.Text = shResult1.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode1.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Display statistics of text 1
-            //StringHelper strInfo = new StringHelper(Text1.Text);
-            //StatText1.Text = strInfo.ToString();
-
-            //// Display statistics of result 1
-            //StringHelper strStatResult1 = new StringHelper(Result1.Text);
-            //StatResult1.Text = strStatResult1.ToString();
-            //// Display statistics of result 2
-            //StringHelper strStatResult2 = new StringHelper(Result2.Text);
-            //StatResult2.Text = strStatResult2.ToString();
         }
 
         private void Text2_Loaded(object sender, RoutedEventArgs e)
         {
             UpdateMainPage();
-
-            //// Check if rows in text 2 is present in text 1. Display difference.
-            //CompareTwoStrings compStrings2 = new CompareTwoStrings(Text2.Text.Trim(), Text1.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult2 = new StringHelper();
-            //Result2.Text = shResult2.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode2.SelectedValue.ToString()), compStrings2.ToString());
-
-            //// Display statistics of text 2
-            //StringHelper strInfo = new StringHelper(Text2.Text);
-            //StatText2.Text = strInfo.ToString();
-
-            //// Display statistics of result 1
-            //StringHelper strStatResult1 = new StringHelper(Result1.Text);
-            //StatResult1.Text = strStatResult1.ToString();
-            //// Display statistics of result 1
-            //StringHelper strStatResult2 = new StringHelper(Result2.Text);
-            //StatResult2.Text = strStatResult2.ToString();
         }
 
         private void SortText1_Click(object sender, RoutedEventArgs e)
@@ -173,36 +99,6 @@ namespace CompareTextsUWP
         private void ResultMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateMainPage();
-
-            //// Check if rows in text 1 is present in text 2. Display difference, format the output as desired "ResultDisplayMode1
-            //CompareTwoStrings compStrings = new CompareTwoStrings(Text1.Text.Trim(), Text2.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult1 = new StringHelper();
-            //Result1.Text = shResult1.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode1.SelectedValue.ToString()), compStrings.ToString());
-
-
-            //// Check if rows in text 2 is present in text 1. Display difference, format the output as desired "ResultDisplayMode2
-            //compStrings = new CompareTwoStrings(Text2.Text.Trim(), Text1.Text.Trim(), MatchCase.IsChecked ?? false);
-            //StringHelper shResult2 = new StringHelper();
-
-            //if (ResultMode2.SelectedValue != null)
-            //    Result2.Text = shResult2.ModifyString((StringHelper.ResultDisplayMode)Enum.Parse(typeof(StringHelper.ResultDisplayMode), ResultMode2.SelectedValue.ToString()), compStrings.ToString());
-            //else
-            //    Result2.Text = "Error";
-
-            //// Display statistics of text 1
-            //StringHelper strInfo = new StringHelper(Text1.Text);
-            //StatText1.Text = strInfo.ToString();
-
-            //// Display statistics of text 2
-            //StringHelper strInfo2 = new StringHelper(Text2.Text);
-            //StatText2.Text = strInfo2.ToString();
-
-            //// Display statistics of result 1
-            //StringHelper strStatResult1 = new StringHelper(Result1.Text);
-            //StatResult1.Text = strStatResult1.ToString();
-            //// Display statistics of result 2
-            //StringHelper strStatResult2 = new StringHelper(Result2.Text);
-            //StatResult2.Text = strStatResult2.ToString();
         }
 
         private void Replace1_Click(object sender, RoutedEventArgs e)
